d5bbdbc69193bc5492a79584e54ae5d9fa6eea69
HajiMohamedRufai <mohamedrufai59@gmail.com>
2023-09-26 22:48:43 +0300

    -m

--- sensor-activity/dags/first_dag.py
+++ sensor-activity/dags/first_dag.py
@@ -1,8 +1,9 @@
 from airflow.decorators import dag, task
 from airflow.sensors.filesystem import FileSensor
-from datetime import datetime
+from datetime import datetime, timedelta
 
 @dag(
+    "first_dag_v0.3",
     schedule=None,
     start_date=datetime(2023, 1, 1),
     tags=['sensor'],
@@ -13,7 +14,8 @@ def first_dag():
     wait_for_files = FileSensor.partial(
         task_id='wait_for_files',
         fs_conn_id='fs_default',
-        mode='reschedule'
+        mode='reschedule',
+        timeout=timedelta(hours=2)
     ).expand(
         filepath=['data_1.csv', 'data_2.csv', 'data_3.csv']
     )
